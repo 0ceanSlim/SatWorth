804592872cc5f198fab16cbcf8a684c48f910cc4
Chris kerr <chriskerr@gmx.com>
2024-06-12 12:31:37 -0400

    updated build version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 33
         versionCode = 7
-        versionName = "0.1.7"
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
